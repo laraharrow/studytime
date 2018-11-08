17b91df0fbd77ceff7068af2b5528242ae636246
laraismael1 <laraismael123@gmail.com>
2018-11-08 11:40:16 -0800

    edit README, now  ready to start project

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <h1>Training for intervews</h1>
+    );
+  }
+}
+
+export default Home;
