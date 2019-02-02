e2df8cbb3f224138af6d0d6c6a4fe4e42dbb505e
laraismael1 <laraismael123@gmail.com>
2019-02-01 18:14:26 -0800

    deploying with heroku

--- src/components/javascript/DataStructure.js
+++ src/components/javascript/DataStructure.js
@@ -255,6 +255,38 @@ const DataStructure = (props) => {
             </Grid>
             <Grid container spacing={24}>
               <Grid item xs={2}>
+                <Paper className={classes.paper}> BST </Paper>
+              </Grid>
+              <Grid item xs={1}>
+                <Paper className={classes.back2}>O(log(n))</Paper>
+              </Grid>
+              <Grid item xs={1}>
+                <Paper className={classes.back2}>O(log(n))</Paper>
+              </Grid>
+              <Grid item xs={1}>
+                <Paper className={classes.back1}>O(log(n))</Paper>
+              </Grid>
+              <Grid item xs={1}>
+                <Paper className={classes.back1}>O(n)</Paper>
+              </Grid>
+              <Grid item xs={1}>
+                <Paper className={classes.back2}>O(n)</Paper>
+              </Grid>
+              <Grid item xs={1}>
+                <Paper className={classes.back2}>O(n)</Paper>
+              </Grid>
+              <Grid item xs={1}>
+                <Paper className={classes.back1}>O(n)</Paper>
+              </Grid>
+              <Grid item xs={1}>
+                <Paper className={classes.back1}>O(n)</Paper>
+              </Grid>
+              <Grid item xs={2}>
+                <Paper className={classes.back1}>O(n)</Paper>
+              </Grid>
+            </Grid>
+            <Grid container spacing={24}>
+              <Grid item xs={2}>
                 <Paper className={classes.paper}> hash table </Paper>
               </Grid>
               <Grid item xs={1}>
@@ -852,37 +884,118 @@ const DataStructure = (props) => {
           <br/>
           <br/>
           <Typography className={classes.align} variant="body1" gutterBottom>
-            This structure like linked list is a sequence of nodes linked one to another. But the similarites end right there. Trees have a hierarchical structure where each node has its own data value a parent and childrens list. Each node can not have more then one parent but it can have muitiple childrens. That is why we refer to it as a childrens list.
+            This structure like linked list is a sequence of nodes linked one to another. But the similarites end right there. Trees have a hierarchical structure where each node has its own data value, a parent node, and a childrens list usually represented by an array of nodes. One more important detail to be aware is that each node can not have more then one parent but it can have muitiple children.
             <br/>
-            One famous tree structure is the <strong className={classes.light}> Document Object Model - DOM </strong> with the <strong className={classes.code}> html </strong> as the root node and <strong className={classes.code}> head </strong> and <strong className={classes.code}> body </strong> as children and every html tags as childrens of the body.
+            One famous tree structure is the <strong className={classes.light}> Document Object Model - DOM </strong> with the <strong className={classes.code}> html </strong> tag as the root node and <strong className={classes.code}> head </strong> and <strong className={classes.code}> body </strong> as its children. With the same relation every tag inside body is part of its childrens list.
             <br/>
-            There are two main ways to traverse through every node in the tree, that is, look into every node in the tree. There are some subdivision that we can get into when we write the code. An interesting note is that each of those traverse are referent to the first structure we looked at <strong className={classes.code}>&nbsp;{Breadth-First Traversal BFS}</strong> uses a stack and <strong className={classes.code}>&nbsp;{depth-First Traversal DFS}</strong> uses a queue to check each node in the tree.
+            There is a very useful Tree, that we will be implementing here, called <strong className={classes.code}>Binary Search Tree - BST</strong>. Everything that we talked about so far relating to trees also apply to a <strong className={classes.code}>BST</strong> tree, exept that every tree parent can only have two children. Both chidren will be placed based on its relationship to the parent tree data. If the new tree introduces a data value greter then its parent tree data. Then that new tree will be placed on the right side of the tree. Now if the new tree has a data value lower then the parent tree data that new tree will be places on the left side.
             <br/>
-            There is a very useful Tree that we will implement here called <strong className={classes.code}>{Binary Search Tree}</strong> and everything that a tree has its applyed to a <strong className={classes.code}>{BST}</strong>. But in this case every node only has 2
+            <strong className={classes.code}>BST</strong> is a great data structure for look up since everytime we have to check for a value you can devide the tree in half. Look back on our Big O table above to see the specific time complexity for a <strong className={classes.code}>Binary Search Tree.</strong>
             <br/>
-            <strong className={classes.code}>&nbsp;{`class Node {`}</strong>
+            There are two main ways to traverse through every node in a <strong className={classes.code}>BST</strong>, that is, look into every node in the tree. There are some subdivision that we can get into when we write the code. But for now we just want to get some understanding of how the two traversing methods work.
+            <br/>
+            <strong className={classes.code}>&nbsp;Breadth-First Traversal - BFT</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;Depth-First Traversal - DFT</strong>
+            <br/>
+            Each of those traverse methods refer to the one of the first structures we looked at. <strong className={classes.code}>BFT</strong> uses a stack and <strong className={classes.code}>DFT</strong> uses a queue to check each node in the tree. Just keep that in mind when we are looking into the code.
+            <br/>
+            <br/>
+            <strong className={classes.code}>&nbsp;{`class Tree {`}</strong>
             <br/>
             <strong className={classes.code}>&nbsp;&nbsp;&nbsp;{`constructor(data) {`}</strong>
             <br/>
             <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.data = data;`}</strong>
             <br/>
-            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.parent = null;`}</strong>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.left = null;`}</strong>
             <br/>
-            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.children = [];`}</strong>
-            <br/>
-            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;{`}`}</strong>
-            <br/>
-            <strong className={classes.code}>&nbsp;{`class Tree {`}</strong>
-            <br/>
-            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;{`root`}</strong>
-            <br/>
-            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.data = data;`}</strong>
-            <br/>
-            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.parent = null;`}</strong>
-            <br/>
-            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.children = [];`}</strong>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.right = null;`}</strong>
             <br/>
             <strong className={classes.code}>&nbsp;&nbsp;&nbsp;{`}`}</strong>
+            <br/>
+            <span className={classes.code}>&nbsp;{`// here we are just creating the blue print of our tree, that would have a value data and the two children: To the left - smaler then the data. and to the right - greter then the data.`}</span>
+            <br/>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;{`insert(data) {`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`if (data <= this.data) {`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.left ? this.left.insert(data): new Tree(data);`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`} else {`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.right ? this.right.insert(data) : new Tree(data);`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`}`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;{`};`}</strong>
+            <br/>
+            <span className={classes.code}>&nbsp;{`/* our insert method, will insert a new node at the correct position based on what is already in our tree.`}</span>
+            <br/>
+            <span className={classes.code}>&nbsp;{`We are first checking if the new value is lower or greter then the data value at root position, that will determine if we go right or left.`}</span>
+            <br/>
+            <span className={classes.code}>&nbsp;{` After that we are checking if there is a child at that position if not we just make the new tree to be placed at that position, otherwise we recursively call insert having the tree at left or right as the root node. That recursion will happend until we find the position where our new tree can be placed.`}</span>
+            <br/>
+            <span className={classes.code}>&nbsp;{`*/`}</span>
+            <br/>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;{`conteins(data) {`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`if (data = this.data) return true;`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`if (data < this.data) {`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.left ? return this.left.contains(data) : return false;`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`} else {`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.right ? return this.right.contains(data) : return false;`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`}`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;{`};`}</strong>
+            <br/>
+            <span className={classes.code}>&nbsp;{`/* Here we are using the same recursive logic as on our insert method.`}</span>
+            <br/>
+            <span className={classes.code}>&nbsp;{` The only difference is that instead of inserting a value we just return true or false based on finding a tree with the expected data.`}</span>
+            <br/>
+            <span className={classes.code}>&nbsp;{` After that we are checking if the parent at that time has a child tree if not we just make the new tree to be placed aas a child of the parent tree at that point, left or right based on the size of data compared with theparent data.`}</span>
+            <br/>
+            <span className={classes.code}>&nbsp;{` Otherwise we recursively call contains having the tree at left or right as the root node (this). That recursion will happend until we find the position where a tree with that data value exists or until we hit an empty child spot.`}</span>
+            <br/>
+            <span className={classes.code}>&nbsp;{`*/`}</span>
+            <br/>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;{`remove(data) {`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`if (data = this.data) return true;`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`if (data < this.data) {`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.left ? return this.left.contains(data) : return false;`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`} else {`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`this.right ? return this.right.contains(data) : return false;`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{`}`}</strong>
+            <br/>
+            <strong className={classes.code}>&nbsp;&nbsp;&nbsp;{`};`}</strong>
+            <br/>
+            <span className={classes.code}>&nbsp;{`/* Here we are using the same recursive logic as on our insert method.`}</span>
+            <br/>
+            <span className={classes.code}>&nbsp;{` The only difference is that instead of inserting a value we just return true or false based on finding a tree with the expected data.`}</span>
+            <br/>
+            <span className={classes.code}>&nbsp;{` After that we are checking if the parent at that time has a child tree if not we just make the new tree to be placed aas a child of the parent tree at that point, left or right based on the size of data compared with theparent data.`}</span>
+            <br/>
+            <span className={classes.code}>&nbsp;{` Otherwise we recursively call contains having the tree at left or right as the root node (this). That recursion will happend until we find the position where a tree with that data value exists or until we hit an empty child spot.`}</span>
+            <br/>
+            <span className={classes.code}>&nbsp;{`*/`}</span>
+            <br/>
+            <br/>
+
+
+
 
 
             <span className={classes.code}>&nbsp;{`b`}</span>
